5061e9dbec002b7daaa6ea6a765450f0b9768127
Choucri FAHED <choucri.fahed@mines-nancy.org>
2019-06-28 16:19:13 +0200

    Change Web3j component URI scheme from 'p6web3j' back to 'web3j'

--- platform6.routes/BlockchainEventHandler/template.groovy
+++ platform6.routes/BlockchainEventHandler/template.groovy
@@ -3,7 +3,7 @@ def contractAddress =  appconfig.get('p6_demo', 'contractAddress')
 
 camel.getCtx().addRoutes(new RouteBuilder() {
     void configure() {
-        from("p6web3j://" + ethClientURL + "?address=" + contractAddress + "&operation=ETH_LOG_FLOWABLE")
+        from("web3j://" + ethClientURL + "?address=" + contractAddress + "&operation=ETH_LOG_FLOWABLE")
             .to("p6cmb://scripts?platform6.request.action=execute&id=p6_demo.BlockchainEventHandler")
             .routeId("p6_demo Web3j - BlockchainEventHandler")
             .description("p6_demo Blockchain Event Handler")
