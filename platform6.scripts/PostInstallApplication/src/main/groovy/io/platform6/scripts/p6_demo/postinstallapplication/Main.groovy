f528e3a1ea67e03bf70a9fff9455bb29a440f9cf
Jonathan Swale <jonathan.swale@gmail.com>
2019-10-16 13:59:55 +0200

    Rename all B2BOX environment variables

--- platform6.scripts/PostInstallApplication/src/main/groovy/io/platform6/scripts/p6_demo/postinstallapplication/Main.groovy
+++ platform6.scripts/PostInstallApplication/src/main/groovy/io/platform6/scripts/p6_demo/postinstallapplication/Main.groovy
@@ -4,7 +4,7 @@ import io.platform6.demo.sc.RequestForQuotations
 import org.web3j.tx.*
 
 // Populate the Items table from the provided CSV file
-String P6_TMP = System.getenv('B2BOX_TMP')
+String P6_TMP = System.getenv('P6_TMP')
 def csvFile = [ skipLines: 0, separator: ',', useFirstLineHeaders: true, uri: "file:///$P6_TMP/p6_demo_items.csv" ]
 
 p6.csv.parse(csvFile) { row ->
