654a1e2eabc003df73cdeef3ac29e74b2012a528
Jonathan Swale <jonathan.swale@gmail.com>
2019-11-21 12:38:38 +0100

    Simplify AppConfig DSL

--- platform6.scripts/PostInstallApplication/src/main/groovy/io/platform6/scripts/p6_demo/postinstallapplication/Main.groovy
+++ platform6.scripts/PostInstallApplication/src/main/groovy/io/platform6/scripts/p6_demo/postinstallapplication/Main.groovy
@@ -15,7 +15,7 @@ p6.csv.parse(csvFile) { row ->
 log.debug 'Populated the Items table from the CSV file'
 
 // Save wallet to interact with smart contract
-p6.appconfig.save("p6_demo", [
+p6.appconfig.save([
     'demoWallet': '{"address":"627306090abab3a6e1400e9345bc60c78a8bef57","id":"c913867f-e5e4-4be6-a7da-654ce833bfb4","version":3,"crypto":{"cipher":"aes-128-ctr","ciphertext":"4c0604cca3314838d63847b4ed02e7a2d17e511efd44f6050e09c238a5d50095","cipherparams":{"iv":"b48d817320ed5b459bbddf140e5f0453"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"35d5339acf6a8f15462ffc0bb981cf319794ae239c51341fb854c18ee458d5c7"},"mac":"76c1a47f7124cd63460ad25fbb0b9c6e3eab8ed50d36f66e2dc40c038881123e"}}'
 ])
 
@@ -29,7 +29,7 @@ def tm = p6.ethereumrpc.pollingTransactionManager(web3j, credentials, 2000L)
 def contract = RequestForQuotations.deploy(web3j, tm, p6.ethereumrpc.DEFAULT_GAS_PROVIDER).send()
 
 // Save the contract address and Ethereum client URL in config table
-p6.appconfig.save("p6_demo", [
+p6.appconfig.save([
     "ethClientURL": ethClientURL,
     "contractAddress": contract.getContractAddress(),
 ])
