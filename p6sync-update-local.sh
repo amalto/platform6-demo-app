10824823f969e37cd91018fafe12ab0e86bcd91d
Choucri FAHED <choucri.fahed@mines-nancy.org>
2020-09-01 20:33:21 +0200

    Update Gradle files and demo app logo

--- p6sync-update-local.sh
+++ p6sync-update-local.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Requires the installation of P6 Sync, usage examples:
-# - Pull modifications: ./p6sync-update-local.sh Dev-Choucri -pull
-# - Push modifications: ./p6sync-update-local.sh Dev-Choucri -push
+# - Pull modifications: ./p6sync-update-local.sh damp-pine-3678 -pull
+# - Push modifications: ./p6sync-update-local.sh damp-pine-3678 -push
 p6sync -app p6_demo -debug -instance $1 -approot . $2 update -p6proxyoverride http://localhost:8480
